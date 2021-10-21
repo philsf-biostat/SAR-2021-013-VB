378cf83a47b6107b2c89f65c029ecab5def32062
Felipe Figueiredo <philsf79@gmail.com>
2021-10-21 19:51:21 -0300

    resultados preliminares

--- scripts/inference.R
+++ scripts/inference.R
@@ -7,35 +7,38 @@
 # tables ------------------------------------------------------------------
 
 # template p-value table
-tab_inf <- analytical %>%
-  # select
-  select(-id, ) %>%
-  tbl_summary(
-    by = group
-  ) %>%
-  # include study N
-  add_overall() %>%
-  # pretty format categorical variables
-  bold_labels() %>%
-  # bring home the bacon!
-  add_p(
-    # use Fisher test (defaults to chi-square)
-    test = all_categorical() ~ "fisher.test",
-    # use 3 digits in pvalue
-    pvalue_fun = function(x) style_pvalue(x, digits = 3)
-  ) %>%
-  # bold significant p values
-  bold_p()
-
-# Template Cohen's D table (obs: does NOT compute p)
 # tab_inf <- analytical %>%
 #   # select
 #   select(-id, ) %>%
 #   tbl_summary(
 #     by = group
 #   ) %>%
-#   add_difference(
-#     test = all_continuous() ~ "cohens_d",
+#   # include study N
+#   add_overall() %>%
+#   # pretty format categorical variables
+#   bold_labels() %>%
+#   # bring home the bacon!
+#   add_p(
+#     # use Fisher test (defaults to chi-square)
+#     test = all_categorical() ~ "fisher.test",
+#     # use 3 digits in pvalue
+#     pvalue_fun = function(x) style_pvalue(x, digits = 3)
 #   ) %>%
-#   modify_header(estimate ~ '**d**') %>%
-#   bold_labels()
+#   # bold significant p values
+#   bold_p()
+
+# Template Cohen's D table (obs: does NOT compute p)
+tab_inf <- analytical %>%
+  mutate(grupo = relevel(grupo, "BQL")) %>%
+  # select
+  # select(-id, ) %>%
+  select(grupo, intensa_t1, intensa_t4) %>%
+  tbl_summary(
+    by = grupo,
+  ) %>%
+  add_difference(
+    # test = all_continuous() ~ "cohens_d",
+  ) %>%
+  bold_p() %>%
+  # modify_header(estimate ~ '**d**') %>%
+  bold_labels()
